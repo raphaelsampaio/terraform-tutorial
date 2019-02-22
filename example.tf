5f0817a63fb579c54126ae3ae2a6136b6c21aa3b
Raphael Sampaio <raphaelsampaio@gmail.com>
2019-02-22 20:13:34 -0300

    Adds an implicitly dependent resource (elastic ip) and an explicitly dependency (IAM instance depends on S3 bucket)

--- example.tf
+++ example.tf
@@ -3,7 +3,17 @@ provider "aws" {
   profile = "admin"
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "raphaelsampaio-terraform-getting-started-guide"
+  acl    = "private"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-076e276d85f524150"
   instance_type = "t2.micro"
+  depends_on    = ["aws_s3_bucket.example"]
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
 }
