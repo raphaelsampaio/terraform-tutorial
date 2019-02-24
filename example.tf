6921f8ad2ef3138bcefc731c31eb0fe31ffbd5d7
Raphael Sampaio <raphaelsampaio@gmail.com>
2019-02-24 13:45:53 -0300

    tests provisioning with the generation of a ip_address.txt, containing the public IP address of the EC2 instance

--- example.tf
+++ example.tf
@@ -3,15 +3,13 @@ provider "aws" {
   profile = "admin"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "raphaelsampaio-terraform-getting-started-guide"
-  acl    = "private"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-076e276d85f524150"
   instance_type = "t2.micro"
-  depends_on    = ["aws_s3_bucket.example"]
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
 
 resource "aws_eip" "ip" {
